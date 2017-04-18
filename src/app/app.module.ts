41f4f36b99618829ae79d0d9c214f92a12f5dd03
Jeroenouw <jeroenouw@hotmail.com>
2017-04-18 15:07:12 +0200

    Components, routing and module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,35 @@ import { NgModule } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule, MdCheckboxModule } from '@angular/material';
+import { MdButtonModule, MdCheckboxModule, MdMenuModule, MdInputModule } from '@angular/material';
+import 'hammerjs';
 
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routing } from "./app.routing";
 
-import { AppComponent } from './components/app.component';
+import { AppComponent } from './app.component';
+import { ContactComponent } from'./contact/contact.component'
+import { HeaderComponent } from "./header.component";
+import { FooterComponent } from "./footer.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MdButtonModule, MdCheckboxModule,
+    MdButtonModule, MdCheckboxModule, MdMenuModule, MdInputModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    Routing
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+}
